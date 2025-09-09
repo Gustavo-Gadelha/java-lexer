10b245a3996c32761b537fbed8a52dea2bba5662
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-09 01:16:07 -0300

    feat: Separa o gerenciamento do Buffer e Lookahead do Lexer

---
+++ src/edu/fafic/lexer/Buffer.java
@@ -0,0 +1,40 @@
+package edu.fafic.lexer;
+
+public class Buffer {
+
+    public final StringBuffer buffer;
+
+    public Buffer() {
+        buffer = new StringBuffer();
+    }
+
+    public void append(int codePoint) {
+        this.buffer.appendCodePoint(codePoint);
+    }
+
+    public void append(Character character) {
+        this.buffer.append(character);
+    }
+
+    public int first() {
+        return this.buffer.codePointAt(0);
+    }
+
+    public int last() {
+        return this.buffer.codePointBefore(buffer.length());
+    }
+
+    public String read() {
+        return this.buffer.toString();
+    }
+
+    public String consume() {
+        String result = this.buffer.toString();
+        this.clear();
+        return result;
+    }
+
+    public void clear() {
+        this.buffer.setLength(0);
+    }
+}
