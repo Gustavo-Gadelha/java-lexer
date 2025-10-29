e7d7489ffce3cd65fb6bc6dcbc56098b118144ca
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-10-29 14:34:40 -0300

    fix: exibe messagem de erro caso não encontre o arquivo ".code" na raiz do projeto

--- src/edu/fafic/Main.java
+++ src/edu/fafic/Main.java
@@ -3,15 +3,23 @@ package edu.fafic;
 import edu.fafic.core.Lexer;
 import edu.fafic.token.Token;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
+        File file = Path.of(".code").toFile();
 
-        try (Reader file = new FileReader(".code")) {
-            Lexer lexer = new Lexer(file);
+        if (!file.exists()) {
+            System.err.println("Arquivo .code não encontrado na raiz do projeto");
+            return;
+        }
+
+        try (Reader reader = new FileReader(file)) {
+            Lexer lexer = new Lexer(reader);
             Token token;
 
             do {
